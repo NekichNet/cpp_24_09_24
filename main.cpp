92610de82ddfe74784b4c0410d9a986cfb2d62a1
NikitaNet <102032304+NekichNet@users.noreply.github.com>
2024-09-25 15:21:04 +0500

    add odd and even sum

--- main.cpp
+++ main.cpp
@@ -5,14 +5,24 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	double sum = 0;
-	double num;
+	int sum = 0;
+	int odd_sum = 0;
+	int even_sum = 0;
+
+	int num;
 	do {
 		std::cout << "¬ведите число: ";
 		std::cin >> num;
 		sum += num;
-	} while (num != 0.0);
-	std::cout << "—умма всех чисел: " << sum;
+		if (num % 2 == 0.0) {
+			even_sum += num;
+		} else {
+			odd_sum += num;
+		}
+	} while (num != 0);
+	std::cout << "—умма всех чисел: " << sum
+		<< "\n—умма чЄтных чисел: " << even_sum
+		<< "\n—умма нечЄтных чисел: " << odd_sum;
 
 	return 0;
 }
